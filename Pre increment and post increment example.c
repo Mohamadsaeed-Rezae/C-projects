73800948f429c2a9d35fe22904e2abd9c53d9a17
Mohamadsaeed <138451483+Mohamadsaeed-Rezae@users.noreply.github.com>
2023-10-09 17:36:56 +0330

    Add files via upload

---
+++ Pre increment and post increment example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a1 =10 ;
+    int a2 = 10 ;
+    int b1 ,b2;
+    b1=++a1 ;
+    b2=a2++ ;
+    printf("a1= %d , b1=%d\n",a1 , b1);
+    printf("a2= %d, b2= %d\n",a2 , b2);
+
+
+    return 0;
+}
